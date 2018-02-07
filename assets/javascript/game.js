71142bd7a9735ff53c153f2e071a4ba2e7629216
jongtelles <jongtelles@gmail.com>
2018-02-06 20:59:14 -0500

    Psuedocode and beginning JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// Predefine variables as undefined to be redefined later 
+var words, currentWord, userGuess, lettersGuessed, lettersMatched, remainingGuesses, wins, availableLetters, win, loss
+
+
+function setup(){
+words = ["words", "that", "are", "part", "of", "the", "theme"]
+console.log(words)
+}
+setup()
+console.log(words)
+
+
+
+// var userGuess = event.key.toLowerCase();
+
+// currentWord = words[Math.floor(Math.random() * words.length)];
+
