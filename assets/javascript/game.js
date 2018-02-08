e108aa897e77b16ea2049fe1debd7ba6ab950941
jongtelles <jongtelles@gmail.com>
2018-02-08 17:14:45 -0500

    Updated javascript to bare minimum functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 // setting initial variables
+var currentWord, numberOfBlanks, blanks, userGuess;
 var words = ["words", "that", "are", "part", "of", "the", "theme"];
 var availableLetters = "abcdefghijklmnopqrstuvwxyz";
 var remainingWords = words.length;
@@ -12,58 +13,62 @@ function gameStart() {
     document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
 };
 gameStart();
-
+// randomly chooses one of the possible words and sets it as the value for currentWord
 function wordPicker() {
-    // randomly chooses one of the possible words and sets it as the value for currentWord
-    var currentWord = words[Math.floor(Math.random() * words.length)];
+    currentWord = words[Math.floor(Math.random() * words.length)];
     console.log(currentWord);
-    // determines number of blanks in currentWord
-    var numberOfBlanks = currentWord.length;
-    console.log(numberOfBlanks);
-    // for each letter in the currentWord, displays a blank space "_" in DOM
-    var blanks = "";
-    for (var i = 0; i < numberOfBlanks; i++) {
-        blanks += "_" + " ";
-        document.getElementById("currentWord").innerHTML = (blanks);
-    };
     //create an array containing the individual characters of currentWord
-    var currentWordArray = currentWord.split('');
+    currentWordArray = currentWord.split('');
     console.log(currentWordArray);
+    // determines number of blanks in currentWord
+    numberOfBlanks = currentWord.length;
+    console.log(numberOfBlanks);
+    // for each letter in the currentWord, add an underscore to the blanks array
+    blanks = [];
+    for (var i = 0; i < numberOfBlanks; i++) {
+        var underscore = "__"
+        blanks.push(underscore);
+        document.getElementById("currentWord").innerHTML = blanks.join("  ");
+    };
 };
 wordPicker();
 // When the user presses a key, 
 document.onkeyup = function (event) {
-    // it will set userGuess to whatever key is pressed, and convert it to lowercase
-    var userGuess = String.fromCharCode(event.which).toLowerCase();
+    // it will set userGuess to whatever key is pressed, convert it to lowercase, add it to the lettersGuessed array and display it
+    userGuess = String.fromCharCode(event.which).toLowerCase();
     console.log(userGuess);
-    // if the user does not enter an A-Z character, will display an error alert
+    lettersGuessed.push(userGuess);
+    document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join("  ");
+    // if the user does not enter an A-Z character, display an error alert
     if (availableLetters.indexOf(userGuess) < 0) {
-        document.getElementById("error").innerHTML = "Please enter a letter from A-Z.";
         alert("Please enter a letter from A-Z.");
     }
     // if an A-Z character is pressed, determine if the userGuess is a part of the currentWord
-    else {
-        // scope issue
-        if (currentWordArray.indexOf(userGuess) >= 0) {
-            // either way, add guessed letter to the lettersGuessed Array
-            lettersGuessed.push(userGuess);
-            // document.getElementById("lettersGuessed").innerHTML = (currentWordArray);
-            // this will eventually write to the DOM adding to "Letters guessed"
-            // lettersGuessed.forEach(function (item, index) {
-            //    document.getElementByID("lettersGuessed").innerHTML = (foo)
-            // });
-            // 
-            console.log(lettersGuessed);
-            //if userGuess doesn't match, remove a remaining guess
-        } else {
-            remainingGuesses--;
+    else if (currentWordArray.indexOf(userGuess) >= 0) {
+        // compares userGuess letter with letters of the currentWord array and replaces them in the DOM if they match
+        for (var i = 0; i < currentWord.length; i++) {
+            if (userGuess === currentWordArray[i]) {
+                blanks[i] = userGuess;
+                console.log(blanks);
+                document.getElementById("currentWord").innerHTML = blanks.join("  ");
+            }
         }
-        // if letter was already guessed, alert("Already guessed! Please try another letter")
-        // if (lettersGuessed.indexOf(userGuess) < 0) {
-        //     alert("Letter already guessed! Please try another letter")
-        // }
-        // letter is not part of current word, subtract a remainingGuess
-    };
-};
+    }
+    //if userGuess doesn't match, remove a remaining guess
+    else if (currentWordArray.indexOf(userGuess) < 0) {
+        remainingGuesses--;
+        document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
+    }
+}
+//check if word is complete
+//check for loss conditions
+// if (remainingGuesses <= 0) {
+//     alert("Game over! YOU LOSE!")
+//     gameStart();
+// }
+// };
 // once a word is successfully guessed, set remainingWords to itself -1 (remainingWords--;)
-// have to check for remaininGuesses =< 0 to trigger game end alert
+
+// // if the letter was already guessed, display error alert
+// else if (lettersGuessed.indexOf(userGuess) >= 0) {
+//     alert("Letter already guessed, please try again!")
