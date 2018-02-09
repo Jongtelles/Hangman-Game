10c24d8005461d2a393e278fa5f95bd17a4e6599
jongtelles <jongtelles@gmail.com>
2018-02-08 21:10:39 -0500

    finalized javascript, updated HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,24 @@
-// setting initial variables
-var currentWord, numberOfBlanks, blanks, userGuess;
-var words = ["words", "that", "are", "part", "of", "the", "theme"];
+// setting global variables
+var currentWord, numberOfBlanks, blanks, userGuess, underscore;
+var words = ["city", "beltway", "east", "coast", "metro", "swamp", "river"];
 var availableLetters = "abcdefghijklmnopqrstuvwxyz";
-var remainingWords = words.length;
 var remainingGuesses = 5;
 var lettersGuessed = [];
 var currentWordArray = [];
 var wins = 0;
-// display number of wins and remaining guesses
+
+// reset variables
 function gameStart() {
+    correctlyGuessedChars = 0;
+    currentWordArray = [];
+    lettersGuessed = [];
+    remainingGuesses = 5;
     document.getElementById("wins").innerHTML = wins;
     document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
+    document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
+    document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
+    document.getElementById("message").innerHTML =  ("Press any key from A-Z to guess!");
+    wordPicker();
 };
 gameStart();
 // randomly chooses one of the possible words and sets it as the value for currentWord
@@ -26,12 +34,12 @@ function wordPicker() {
     // for each letter in the currentWord, add an underscore to the blanks array
     blanks = [];
     for (var i = 0; i < numberOfBlanks; i++) {
-        var underscore = "__"
+        underscore = "__"
         blanks.push(underscore);
         document.getElementById("currentWord").innerHTML = blanks.join("  ");
     };
 };
-wordPicker();
+
 // When the user presses a key, 
 document.onkeyup = function (event) {
     // it will set userGuess to whatever key is pressed, convert it to lowercase, add it to the lettersGuessed array and display it
@@ -49,26 +57,23 @@ document.onkeyup = function (event) {
         for (var i = 0; i < currentWord.length; i++) {
             if (userGuess === currentWordArray[i]) {
                 blanks[i] = userGuess;
-                console.log(blanks);
                 document.getElementById("currentWord").innerHTML = blanks.join("  ");
             }
         }
+        correctlyGuessedChars++;
     }
-    //if userGuess doesn't match, remove a remaining guess
+    // if userGuess doesn't match, remove a remaining guess
     else if (currentWordArray.indexOf(userGuess) < 0) {
         remainingGuesses--;
         document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
     }
+    // checking for win or loss
+    if (correctlyGuessedChars === currentWord.length) {
+        document.getElementById("error").innerHTML = ("You WIN!");
+        wins++;
+        gameStart();
+    } else if (remainingGuesses === 0) {
+        document.getElementById("error").innerHTML = ("You LOSE! Try again!");
+        gameStart();
+    }
 }
-//check if word is complete
-//check for loss conditions
-// if (remainingGuesses <= 0) {
-//     alert("Game over! YOU LOSE!")
-//     gameStart();
-// }
-// };
-// once a word is successfully guessed, set remainingWords to itself -1 (remainingWords--;)
-
-// // if the letter was already guessed, display error alert
-// else if (lettersGuessed.indexOf(userGuess) >= 0) {
-//     alert("Letter already guessed, please try again!")
